8f382c46eb70a279353b35ac59b3a717d21953df
Quigley <quigley@resurnate.com>
2024-01-05 22:16:18 -0600

    Added 2024001 comic strip

--- js/resurnate.metadata.js
+++ js/resurnate.metadata.js
@@ -1,8 +1,19 @@
 const galleryMetadata = [
   {
+    id:'2024001',
+    image:'2024001.png',
+    title:'Gone Vaccinated',
+    description:'I had this brilliant idea to create a slideshow of each panel in the comic strip. I even went as ' +
+        'far as creating a tool to build a video. The goal was to dump it as a Facebook Reel, but I wanted to play ' +
+        'with something different. I thought, &#34;Hey, what about TikTok?&#34;. I downloaded the app and within ' +
+        'minutes I created a slideshow, complete with music. I really didn&#39;t know how it all worked. Within a ' +
+        'blink of an eye I was notified about activity regarding the post. I did have to laugh; hence, out of about ' +
+        'a thousand views, it had got a poultry few likes and a follower or two. Talk about gone vaccinated!'
+  },
+  {
     id:'2023002',
     image:'2023002.png',
-    title:'The Equalizer',
+    title:'Home Front Disorder',
     description:'I&#39;m fairly certain anyone that isn&#39;t a hermit living under a rock, can relate. However, ' +
         'this does seem to impact the fairer sex more. I&#39;ll leave that up to you to decide who that fairer sex ' +
         'is... shots fired! Unfortunately, this is exactly what happened the other night, with &#34;It&#39;s called ' +
